3132f0496f99bab34d83ec59de8bf60c00dd796a
KulaniBaloyi <kulani17.kb@gmail.com>
2023-11-03 19:05:01 +0200

    changed config file

--- app/components/BlogPost.js
+++ app/components/BlogPost.js
@@ -1,13 +1,13 @@
 const BlogPost = () => {
   return (
-    <div className="rounded-lg border flex flex-col overflow-hidden">
+    <article className="rounded-lg border flex flex-col overflow-hidden">
          <section className=" bg-gray-200 w-full h-full aspect-video"></section>
          <section className="bg-white p-5 flex flex-col gap-5">
             <h1>First Blog Post</h1>
             <h2>Date</h2>
             <p>Short Description of the blog post where i will be rambling on for about this length about nothing really</p>
          </section>
-    </div>
+    </article>
    
   )
 }
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,7 @@
 const Projects = () => {
   return (
     <div className='min-w-[300px] flex flex-col space-y-10 pb-10  w-[90%] lg:w-[80%] m-auto mt-[5.2rem]'>
+
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 ">
             <div className="border rounded-sm w-full aspect-square"></div>
             <div className="border rounded-sm w-full aspect-square"></div>
