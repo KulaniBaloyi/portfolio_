d7d8d0534fee950eb72546ced43a8f7a45e4946b
KulaniBaloyi <kulani17.kb@gmail.com>
2023-11-11 01:26:08 +0200

    fetching individual blog post dynamically

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,33 +1,77 @@
 import { client } from "@/lib/contentful"
 import Image from "next/image"
 
-
+let newdata
 const IndividualBlog = async({params}) => {
-  let data
-  const slug = decodeURIComponent(params.slug)
-  const title = slug.replace(/-/g, " ")
- 
+  const slug = params.slug
+  const title = decodeURIComponent(slug.replace(/-/g, " "))
   const id = params.id
 
-//   try{
-//     const res = await client.getEntries({
-//       'content_type': "BlogPost",
-//       'fields.slug[in]': "the-future-of-cybersecurity-how-were-protecting-ourselves-in-a-digital-world",
+  try{
+    const res = await client.getEntries({
+      "content_type": "blogPost",
+       "fields.slug[in]": slug,
     
-//   })
-//   console.log("slug : ", res)
-// }  
-// catch(err){
-//  console.log("something went wrong!!!", err)
+  })
+  newdata = res?.items.map((item)=>{
+    const {sys,fields}=item
+    const {id} =sys
+    console.log("fields: ",fields)
+    const title = fields.title
+    const slug = fields.slug
+    const readTime = fields.readTime
+    const summary = fields.summary
+    const intro = fields.introduction
+    const conclusion = fields.conclusion
+    const tableOfContents = fields.tableOfContents
+    //console.log("table of contents: ", tableOfContents)
 
-//   }
+    const coverImage = fields.coverImage.fields.file.url
+    const blog = {id,title,readTime,coverImage,slug,intro,conclusion,summary,tableOfContents}
+    console.log("blog: ", blog)
+
+   
+     return (
+       blog
+     )
+     
+   })
+  console.log("slug : ", res)
+}  
+catch(err){
+ console.log("something went wrong!!!", err)
+
+  }
  
   return (
     <div className='min-h-screen flex flex-col mt-[5.2rem] gap-10 text-3xl w-[90%]  lg:w-4/5 mx-auto pb-10'>
       <h1 className="leading-[1.5em] text-5xl font-bold text-center mt-[1em] capitalize">{title}</h1>
-      <section className="h-[500px] w-full relative border rounded-lg bg-neutral-950/10">
-        {/* <Image fill src={data[0]?.coverImage} alt={data[0].title}/> */}
+      {newdata&&newdata.map(d=>{
+          return(
+            <>
+           
+             <section className="h-[500px] w-full relative border rounded-lg overflow-hidden">
+        
+             <Image fill src={`https://${d.coverImage}`} alt={title} priority className="object-cover origin-center"/>
       </section>
+      <p>read time {d.readTime}min</p>
+      <p className="text-sm text-gray-600">Summary</p>
+      <p>{d.summary}</p>
+      <p>Topics covered: </p>
+      {d.tableOfContents.map((item,index)=>{
+        return<>
+        
+        <p className="text-sm text-gray-600" key={index}>{item}</p></> 
+
+      })}
+     
+      <p>{d.intro}</p>
+      <p>{d.conclusion}</p>
+            
+            </>
+          )
+        })}
+     
     </div>
   )
 }
