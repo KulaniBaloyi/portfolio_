4411c9315452400b8b9eab9201ce36afda7776c2
KulaniBaloyi <kulani17.kb@gmail.com>
2023-11-10 23:55:15 +0200

    connected contentful and dynamically fetching blog posts

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -2,24 +2,26 @@ import { client } from "@/lib/contentful"
 import Image from "next/image"
 
 
-const Page = async({params}) => {
+const IndividualBlog = async({params}) => {
   let data
   const slug = decodeURIComponent(params.slug)
   const title = slug.replace(/-/g, " ")
  
   const id = params.id
 
-  try{
-    const res = await client.getEntryBySlug(slug)  
-    console.log('slug: ', res)
-     
-  }
-     
-catch(err){
-
-  }
- 
+//   try{
+//     const res = await client.getEntries({
+//       'content_type': "BlogPost",
+//       'fields.slug[in]': "the-future-of-cybersecurity-how-were-protecting-ourselves-in-a-digital-world",
     
+//   })
+//   console.log("slug : ", res)
+// }  
+// catch(err){
+//  console.log("something went wrong!!!", err)
+
+//   }
+ 
   return (
     <div className='min-h-screen flex flex-col mt-[5.2rem] gap-10 text-3xl w-[90%]  lg:w-4/5 mx-auto pb-10'>
       <h1 className="leading-[1.5em] text-5xl font-bold text-center mt-[1em] capitalize">{title}</h1>
@@ -30,5 +32,5 @@ catch(err){
   )
 }
 
-export default Page
+export default IndividualBlog
 
