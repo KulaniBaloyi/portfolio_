817bb982a1a8817010bf35fcd7ff2b62f78d93ea
KulaniBaloyi <kulani17.kb@gmail.com>
2023-12-07 13:50:39 +0200

    changed layout of blogpost page

--- app/components/ContentfulImage.js
+++ app/components/ContentfulImage.js
@@ -5,7 +5,6 @@ const contentfulLoader = ({ src, width, quality,alt }) => {
   return `${src}?w=${width}&q=${quality || 75}`
 }
 
-
 const ContentfulImage = props => {
   return <Image alt={props.alt} loader={contentfulLoader} {...props} />
 }
