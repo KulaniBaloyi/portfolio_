6f29bf50f5ae78a7ac945366c8da5749c3fadc71
KulaniBaloyi <kulani17.kb@gmail.com>
2023-12-14 23:44:39 +0200

    changed layout of blogpost page

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -74,6 +74,7 @@ catch(err){
       <div className='mx-auto prose'>
       <RichText content={d.content} />
     </div>
+    
  <p className="mx-auto prose">{d.conclusion}</p>  
         </p>
         <p className="border p-5">ghgfh</p>
