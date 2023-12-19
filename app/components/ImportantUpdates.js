f6171bfc8828d5e8bf69e0551b35c74d203d5d65
KulaniBaloyi <kulani17.kb@gmail.com>
2023-12-19 22:00:47 +0200

    updated next14

--- app/components/ImportantUpdates.js
+++ app/components/ImportantUpdates.js
@@ -114,7 +114,6 @@ const ImportantUpdates = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="text-gray-400 group-hover:translate-x-1 ease-in-out transition-all duration-150 w-5 h-5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                         </svg>
-
                     </div>
                 </li> */}
             </ul>
